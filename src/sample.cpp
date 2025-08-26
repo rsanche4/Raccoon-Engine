324c0f6e572afea68be27bc1b0142d7139804dc2
Rafael Sanchez <rsanzek25@gmail.com>
2025-08-25 23:04:23 -0400

    added more

--- src/sample.cpp
+++ src/sample.cpp
@@ -1,8 +1,35 @@
 #include <SDL3/SDL.h>
 #include <cmath>
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 
 using namespace std;
+
+// Globals
+int screen_width = 320;
+int screen_height = 240;
+int application_width = screen_width*4;
+int application_height = screen_height*4;
+
+// Player Stats
+float player_pos[3] = {0, 0, 400};
+float player_dir = 4.7124;
+float fp_dist = 400;
+
+// read map file
+int readLevel(const string& filename) {
+    ifstream file(filename);
+    string line;
+    
+    while (getline(file, line)) {
+        cout << line << endl;
+        // CONTINUE HERE
+    }
+    
+    return 0;
+}
 
 // Get the euclid distance given 2 points in 2D plane
 float euclid_dist(float v2x, float v2y, float v1x, float v1y) {
@@ -20,17 +47,41 @@ float projectY(float vertexy, float vertexz, float fpy, float fpz, float focal_p
     return pixelY;
 }
 
-int main() {
-    //int answer = projectY(20, -5, 0, 5, 5, 8);
-    //cout << answer;
+// Function to draw pixels directly per column
+void drawPixels(Uint32* pixels, int pitch, unsigned int frames) {
+    // Example: Draw some colored lines with gradients
 
+    for (int x = 0; x < screen_width; x++) {
+        for (int y = 0; y < screen_height; y++) {
+            Uint32 color = 0xFF000000; //A R G B
+            
+            Uint32 a = 0xFF000000;
+            Uint32 r = (x % 256) << 16;
+            Uint32 g = (y % 256) << 8;
+            Uint32 b = ((x+y) % 256);
+
+            color = a | r | g | b;
+
+            pixels[y * pitch + x] = color;
+        }
+    }
+}
+
+int main() {
+    readLevel("level_test.txt");
+    
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window* w = SDL_CreateWindow("Box", 400, 300, 0);
+    SDL_Window* w = SDL_CreateWindow("Raccoon Engine", application_width, application_height, 0);
     SDL_Renderer* r = SDL_CreateRenderer(w, NULL);
     
-    float x = 200;
+    // Create surface at low resolution for fast pixel manipulation
+    SDL_Surface* surface = SDL_CreateSurface(screen_width, screen_height, SDL_PIXELFORMAT_RGBA8888);
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(r, surface);
+    
+    float x = 200; // just boilerplate for now
     SDL_Event e;
     
+    unsigned int frames = 0;
     while (1) {
         while (SDL_PollEvent(&e)) {
             if (e.type == SDL_EVENT_QUIT) break;
@@ -41,15 +92,28 @@ int main() {
         if (keys[SDL_SCANCODE_LEFT] && x > 0) x -= 5;
         if (keys[SDL_SCANCODE_RIGHT] && x < 350) x += 5;
         
-        SDL_SetRenderDrawColor(r, 0, 0, 0, 255);
+        // Lock surface for direct pixel access
+        SDL_LockSurface(surface);
+        Uint32* pixels = (Uint32*)surface->pixels;
+        int pitch = surface->pitch / 4; // pitch in pixels, not bytes
+        
+        // Call your pixel drawing function
+        drawPixels(pixels, pitch, frames);
+        
+        SDL_UnlockSurface(surface);
+        
+        // Update texture and render scaled up to full application size
+        SDL_UpdateTexture(texture, NULL, surface->pixels, surface->pitch);
         SDL_RenderClear(r);
-        SDL_SetRenderDrawColor(r, 255, 255, 255, 255);
-        SDL_FRect box = {x, 150, 50, 50};
-        SDL_RenderFillRect(r, &box);
+        SDL_RenderTexture(r, texture, NULL, NULL); // This automatically scales up
         SDL_RenderPresent(r);
         SDL_Delay(16);
+
+        frames++;
     }
     
+    SDL_DestroySurface(surface);
+    SDL_DestroyTexture(texture);
     SDL_Quit();
     return 0;
 }
